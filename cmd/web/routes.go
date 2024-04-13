1a1f31b6fc97f9261a19668e0808a39c39df7af0
nadiannis <annisanadianeyla@gmail.com>
2024-04-13 22:47:20 +0700

    feat: serve static files

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,9 +1,16 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/nadiannis/stn/ui"
+)
 
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.FS(ui.Files))
+	mux.Handle("/static/", fileServer)
 
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/links/list", app.linkList)
